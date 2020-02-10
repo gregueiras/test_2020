a6d2849fca644ea4a817fd8553a9714c38b63ccf
Pedro <pedrorodbarradas@gmail.com>
2020-02-10 17:10:30 +0000

    1 commit

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send({name: "Hello World"}));
+app.get("/", (req, res) => res.send({name: "Hello!!!"}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
